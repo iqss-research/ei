c814a3c593e97f6bc375343cb16a614ec70358b3
Molly Roberts <molly.e.roberts@gmail.com>
2010-08-04 18:44:39 +0000

    Added documentation.  Changed get() to eiread().  Created plot() and summary) functions.

--- ei/R/ei.R
+++ ei/R/ei.R
@@ -3,44 +3,9 @@ library(msm)
 library(mvtnorm)
 library(tmvtnorm)
 
-samp <- function(t,x,n, Zb, Zw, par, varcv, nsims, keep, numb, covs, erho, esigma, ebeta, ealphab, ealphaw){
-#a <- t(chol(varcv))
-#mat <- matrix(rnorm(5*nsims), nrow=5)
-#draw <- t(par + a%*%mat)
-import1 <- NULL
-varcv2 <- solve(varcv)/4
-draw <- rmvnorm(nsims, par[covs], varcv2)
-varcv3 <- solve(varcv2)
-phiv <- dmvnorm(draw, par[covs], varcv3, log=T)
-zbmiss <- ifelse(covs[6:(5+numb)]==FALSE,TRUE,FALSE)
-zwmiss <- ifelse(covs[(6+numb):length(covs)]==FALSE, TRUE, FALSE)
-if(zbmiss==TRUE&zwmiss==FALSE){
-	draw <- cbind(draw[,1:5], rep(1,nsims), draw[,(5+numb):sum(covs)])
-	}
-if(zbmiss==FALSE&zwmiss==TRUE){
-	draw <- cbind(draw, rep(1,nsims))
-	}
-if(zbmiss==TRUE&zwmiss==TRUE){
-	draw <- cbind(draw, rep(1,nsims), rep(1,nsims))
-	}
-for(i in 1:nsims){
-#ymu <- draw[i,] - par
-#phiv = -(t(as.matrix(ymu))%*%solve(varcv)%*%as.matrix(ymu))/2
-import1[i] <- like(as.vector(draw[i,]), t, x, n, Zb, Zw, numb=numb, erho, esigma, ebeta, ealphab, ealphaw) - phiv[i]
-}
-lnir <- import1-max(import1[1:nsims])
-ir <- exp(lnir)
-tst <- ir[1:nsims]>runif(nsims,0,1)
-keep <- rbind(keep, draw[tst,])
-return(keep)
-}
 
-## commented out
-## Zb <- as.matrix(data$x)
-## Zw <- as.matrix(data$x)
-
-#ei1 <- ei(t,x,n,Zb,Zw,erho=.5,esigma=.5,ebeta=0,ealphab=NA,ealphaw=NA)
-ei <- function(t,x,n,Zb,Zw, erho, esigma, ebeta, ealphab, ealphaw){
+#ei1 <- ei(t,x,n,Zb,Zw,erho=.5,esigma=.5,ebeta=0,ealphab=NA,ealphaw=NA, truth=NA)
+ei <- function(t,x,n,Zb,Zw, erho=.5, esigma=.5, ebeta=0, ealphab=NA, ealphaw=NA, truth=NA){
 Zb <- as.matrix(Zb)
 Zw <- as.matrix(Zw)
 numb <- dim(Zb)[2]
@@ -50,7 +15,6 @@ solution <- optim(start, like, y=t, x=x, n=n, Zb=Zb, Zw=Zw,numb=numb, erho=erho,
 covs <- as.logical(ifelse(diag(solution$hessian)==0,0,1))
 varcv <- -solution$hessian[covs,covs]
 #varcv <- varcv
-
 
 keep <- matrix(data=NA, ncol=(length(solution$par)))
 resamp <- 0
@@ -112,72 +76,49 @@ mbetab <- apply(betab,1,mean)
 mbetaw <- apply(betaw,1,mean)
 sdbetab <- apply(betab,1,sd)
 sdbetaw <- apply(betaw,1,sd)
-output <- list(solution$par, solution$hessian, psi, mbetab, mbetaw, sdbetab, sdbetaw, betab, betaw, resamp, erho, esigma, ebeta, ealphab, ealphaw, numb, x, t, n)
-names(output) <- c("phi", "hessian", "psi", "betab", "betaw", "sbetab", "sbetaw", "betabs", "betaws", "resamp", "erho", "esigma", "ebeta", "ealphab", "ealphaw", "numb", "x", "t", "n")
+output <- list(solution$par, solution$hessian, psi, mbetab, mbetaw, sdbetab, sdbetaw, betab, betaw, resamp, erho, esigma, ebeta, ealphab, ealphaw, numb, x, t, n, Zb, Zw, truth)
+names(output) <- c("phi", "hessian", "psi", "betab", "betaw", "sbetab", "sbetaw", "betabs", "betaws", "resamp", "erho", "esigma", "ebeta", "ealphab", "ealphaw", "numb", "x", "t", "n", "Zb", "Zw", "truth")
 class(output) <- "ei"
 return(output)
 }
 
-summary.ei <- function(ei1){
-#Calculate maximum likelihood results in the scale of estimation
-numb <- ei1$numb
-covs <- as.logical(ifelse(diag(ei1$hessian)==0,0,1))
-sdphi <- diag(sqrt(solve(-ei1$hessian[covs,covs])))
+samp <- function(t,x,n, Zb, Zw, par, varcv, nsims, keep, numb, covs, erho, esigma, ebeta, ealphab, ealphaw){
+#a <- t(chol(varcv))
+#mat <- matrix(rnorm(5*nsims), nrow=5)
+#draw <- t(par + a%*%mat)
+import1 <- NULL
+varcv2 <- solve(varcv)/4
+draw <- rmvnorm(nsims, par[covs], varcv2)
+varcv3 <- solve(varcv2)
+phiv <- dmvnorm(draw, par[covs], varcv3, log=T)
 zbmiss <- ifelse(covs[6:(5+numb)]==FALSE,TRUE,FALSE)
 zwmiss <- ifelse(covs[(6+numb):length(covs)]==FALSE, TRUE, FALSE)
-names <- c("Bb0", "Bw0", "sigB", "sigW", "rho")
 if(zbmiss==TRUE&zwmiss==FALSE){
-	sdphi <- c(sdphi[1:5], 0, sdphi[(5+numb):sum(covs)])
-	numw <- length(ei1$phi) - (5+numb)
-	wname <- NULL
-	for (i in 1:numw){
-	wname[i] = paste("Zw",(i-1), sep="")
-	}
-	names <- c(names, "Zb0", wname)
+	draw <- cbind(draw[,1:5], rep(1,nsims), draw[,(5+numb):sum(covs)])
 	}
 if(zbmiss==FALSE&zwmiss==TRUE){
-	sdphi <- c(sdphi, 0)
-	bname <- NULL
-	for (i in 1:numb){
-	bname[i] = paste("Zb",(i-1), sep="")
-	}
-	names <- c(names, bname, "Zw0")
+	draw <- cbind(draw, rep(1,nsims))
 	}
 if(zbmiss==TRUE&zwmiss==TRUE){
-	sdphi <- c(sdphi, 0, 0)
-	names <- c(names, "Zb0", "Zw0")
+	draw <- cbind(draw, rep(1,nsims), rep(1,nsims))
 	}
-mle <- as.data.frame(rbind(ei1$phi, sdphi))
-names(mle) <- names
+for(i in 1:nsims){
+#ymu <- draw[i,] - par
+#phiv = -(t(as.matrix(ymu))%*%solve(varcv)%*%as.matrix(ymu))/2
+import1[i] <- like(as.vector(draw[i,]), t, x, n, Zb, Zw, numb=numb, erho, esigma, ebeta, ealphab, ealphaw) - phiv[i]
+}
+lnir <- import1-max(import1[1:nsims])
+ir <- exp(lnir)
+tst <- ir[1:nsims]>runif(nsims,0,1)
+keep <- rbind(keep, draw[tst,])
+return(keep)
+}
 
 
-#Calculate untruncated psis
-n <- length(ei1$betab)
-BB <- mean(ei1$psi[,1:n])
-BW <- mean(ei1$psi[,(n+1):(2*n)])
-SB <- mean(ei1$psi[,((2*n)+1)])
-SW <- mean(ei1$psi[,((2*n)+2)])
-RHO <- mean(ei1$psi[,((2*n)+3)])
-psiu <- as.data.frame(t(c(BB,BW,SB,SW,RHO)))
-names(psiu) <- c("BB","BW", "SB", "SW", "RHO")
-
-#Calculate truncated psis
-BB <- mean(ei1$betabs)
-BW <- mean(ei1$betaws)
-SB <- sd(as.vector(ei1$betabs))
-SW <- sd(as.vector(ei1$betaws))
-RHO <- cor(as.vector(ei1$betabs), as.vector(ei1$betaws))
-psit <- as.data.frame(t(c(BB,BW,SB,SW,RHO)))
-names(psit) <- c("BB","BW", "SB", "SW", "RHO")
-
-#Aggregate Bounds
-ab <- matrix(abounds(ei1$x, ei1$t, ei1$n), nrow=2)
-
-
-#Estimates of Aggregate Quantities of Interest
-magg <- matrix(maggs(ei1$x,ei1$t,ei1$n, ei1$betabs, ei1$betaws), nrow=2)
-
-output <- list(ei1$erho, ei1$esigma, ei1$ebeta, n, ei1$resamp, mle, psiu, psit, ab, magg)
-names(output) <- c("Erho", "Esigma", "Ebeta", "N", "Resamp", "MLE", "psiu", "psit", "ab", "magg")
-return(output)
+print.ei <- function(ei.object, ...){
+message("resamp")
+message(ei.object$resamp)
+message("Maximum likelihood results in scale of estimation")
+print(ei.object$phi)
+message("Use summary() and get() for more quantities of interest.")
 }
--- ei/R/eiread.R
+++ ei/R/eiread.R
@@ -1,93 +1,19 @@
-#Quantities of Interest
+eiread <- function(ei.object, ...){
+	function.list <- list("betab" = betaB, "betaw" = betaW, "phi" = phi, "sbetab" = sbetab, "sbetaw" = sbetaw, "psisims" = psisims, "bounds" = bounds, "CI80b" = CI80b, "CI80w" = CI80w, "abounds" = abounds, "aggs" = aggs, "maggs" = maggs, "VCaggs" = VCaggs, "eaggbias" = eaggbias, "goodman" = goodman)
+	arguments <- list(...)
+	results <- list()
+	
+for (arg in arguments) {
+	if (arg %in% names(function.list))
+		results[[arg]] <- function.list[[arg]](ei.object)
+	else 
+		results[[arg]] <- NA
+}
 
-abounds <- function(x,t,n){
-	bounds <- bounds1(x,t,n)
-	omx <- 1-x
-	Nb <- n*x
-	Nw <- n*omx
-	LAbetaB <- weighted.mean(bounds[,1],Nb)
-	UAbetaB <- weighted.mean(bounds[,2], Nb)
-	LAbetaW <- weighted.mean(bounds[,3], Nw)
-	UAbetaW <- weighted.mean(bounds[,4], Nw)
-	return(c(LAbetaB, UAbetaB, LAbetaW,UAbetaW))	
-	}
+	if (length(results) <1)
+	warning("qi results object is empty")
+
+	class(results) <- "eiread"
 	
-aggs <- function(x,t,n, betab, betaw){
-	betab <- as.matrix(betab)
-	betaw <- as.matrix(betaw)
-	omx <- 1-x
-	Nb <- n*x
-	Nw <- n*omx
-	Bbgg <- vector(mode="numeric", length=dim(betab)[2])
-	for (i in 1:dim(betab)[2]){
-		Bbgg[i] <- weighted.mean(betab[,i], Nb)
-		}
-	Bwgg <- vector(mode="numeric", length=dim(betaw)[2])
-	for (i in 1:dim(betaw)[2]){
-		Bwgg[i] <- weighted.mean(betaw[,i], Nw)
-		}
-	return(cbind(Bbgg, Bwgg))
-	}
-	
-maggs <- function(x,t,n, betab, betaw){
-	omx <- 1-x
-	Nb <- n*x
-	Nw <- n*omx
-	betab <- as.matrix(betab)
-	betaw <- as.matrix(betaw)
-	Bbgg <- vector(mode="numeric", length=dim(betab)[2])
-	for (i in 1:dim(betab)[2]){
-		Bbgg[i] <- weighted.mean(betab[,i], Nb)
-		}
-	Bwgg <- vector(mode="numeric", length=dim(betaw)[2])
-	for (i in 1:dim(betaw)[2]){
-		Bwgg[i] <- weighted.mean(betaw[,i], Nw)
-		}
-	return(c(mean(Bbgg), mean(Bwgg), sd(Bbgg), sd(Bwgg)))
-	}
-	
-VCaggs <- function(x,t,n,betab, betaw){
-	betab <- as.matrix(betab)
-	betaw <- as.matrix(betaw)
-	omx <- 1-x
-	Nb <- n*x
-	Nw <- n*omx
-	Bbgg <- vector(mode="numeric", length=dim(betab)[2])
-	for (i in 1:dim(betab)[2]){
-		Bbgg[i] <- weighted.mean(betab[,i], Nb)
-		}
-	Bwgg <- vector(mode="numeric", length=dim(betaw)[2])
-	for (i in 1:dim(betaw)[2]){
-		Bwgg[i] <- weighted.mean(betaw[,i], Nw)
-		}
-	vc <- matrix(c(var(Bbgg), cov(Bbgg, Bwgg), cov(Bbgg, Bwgg), var(Bwgg, Bwgg)), nrow=2)
-	return(vc)
-	}
-	
-CI80b <- function(betab){
-	lwr <- apply(betab, 1, function(x) quantile(x, probs=c(.1)))
-	upr <-apply(betab, 1, function(x) quantile(x, probs=c(.9)))
-	return(cbind(lwr,upr))
-	}
-	
-CI80w <- function(betaw){
-	lwr <- apply(betaw, 1, function(x) quantile(x, probs=c(.1)))
-	upr <-apply(betaw, 1, function(x) quantile(x, probs=c(.9)))
-	return(cbind(lwr,upr))
-	}
-	
-eaggbias <- function(x, mbetab, mbetaw){
-	lm.b <- lm(mbetab ~ x)
-	lm.w <- lm(mbetaw ~ x)
-	return(rbind(summary(lm.b)$coefficients, summary(lm.w)$coefficients))
-	}
-	
-goodman <- function(t, x){
-	lm.g <- lm(t ~ x)
-	w <- 1-x
-	lm.w <- lm(t ~ w)
-	BetaW <- summary(lm.g)$coefficients[1,]
-	BetaB <- summary(lm.w)$coefficients[1,]
-	coefs <- rbind(BetaB, BetaW)
-	return(coefs)
-	}
+	results
+}
