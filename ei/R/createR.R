0837919d8fedb5ce149fa3dfc0aa0be86c9d0fae
Molly Roberts <molly.e.roberts@gmail.com>
2010-09-06 21:18:11 +0000

    createR added

---
+++ ei/R/createR.R
@@ -0,0 +1,39 @@
+library(mnormt)
+library(cubature)
+createR <- function(sub, Rfun){
+out <- NULL
+lower = cbind(-bb[sub]/sb, -bw[sub]/sw)
+upper = cbind(-bb[sub]/sb+1/sb, -bw[sub]/sw+1/sw)
+mean=c(0,0)
+corr=matrix(c(1,rho,rho,1),nrow=2)
+#pmvnorm
+
+if (Rfun==1){
+	for(i in 1:length(x[sub])){
+		qi <- pmvnorm(lower=lower[i,], upper=upper[i,], mean=mean, corr=corr)
+		qi <- ifelse(qi<0|qi==0, 1*10^-322,qi)
+		out[i] <- log(qi)
+		out[i] <- ifelse(is.na(out[i])|abs(out[i]==Inf), 999, out[i])
+		}
+	return(out)
+	}
+
+if (Rfun==2){
+	for(i in 1:length(x[sub])){
+		qi <- sadmvn(lower=lower[i,], upper=upper[i,], mean=mean, varcov=corr)
+		qi <- ifelse(qi<0|qi==0, 1*10^-322,qi)
+		out[i] <- log(qi)
+		out[i] <- ifelse(is.na(out[i])|abs(out[i]==Inf), 999, out[i])
+		}
+	return(out)
+	}
+if (Rfun==3){
+	fun <- function(x) dmvnorm(x,mean,corr)
+	for (i in 1:length(x[sub])){
+		qi <- adaptIntegrate(fun, lower=lower[i,], upper=upper[i,])$integral
+		out[i] <- log(qi)
+		out[i] <- ifelse(is.na(out[i])|abs(out[i]==Inf), 999, out[i])
+		}
+	return(out)
+	}
+}	
