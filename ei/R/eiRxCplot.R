8929a55ce0cd20517fa1e84a383d6494ac3f38bf
Molly Roberts <molly.e.roberts@gmail.com>
2011-05-18 15:45:49 +0000

    adding eiRxCplot

---
+++ ei/R/eiRxCplot.R
@@ -0,0 +1,57 @@
+#Plot to help visualize multiple dimensions.
+
+eiRxCplot <- function(ei.object, random =FALSE, black=FALSE, hispanic=FALSE,white=FALSE,informative=FALSE, title,xaxis, yaxis, percent, data){
+  ok <- !is.na(ei.object$betab)&!is.na(ei.object$betaw)
+  x <- ei.object$x[ok]
+  t <- ei.object$t[ok]
+  n <- ei.object$n[ok]
+  bounds <- bounds1(x,t,n)
+  bbounds <- cbind(bounds[,1], bounds[,2])
+  wbounds <- cbind(bounds[,4], bounds[,3])
+  bounds3 <- na.omit(bounds)
+  unan <- sum(bounds3[,1]==bounds3[,2] & bounds3[,1]==bounds3[,3] & bounds3[,1]==bounds3[,4] & bounds3[,1]==1)
+  n <- dim(bounds)[1]
+  betabs <- ei.object$betabs[ok,]
+  betaws <- ei.object$betaws[ok,]
+  plot(c(100,200), xlim=c(0,1), ylim=c(0,1),
+       col="white", xaxs="i",
+       yaxs="i", main=title, xlab=xaxis, ylab=yaxis)
+ok2 <- NULL
+ rand <- rep(TRUE, length(x[ok]))
+  if (random ==TRUE) {
+       rand <- sample(c(TRUE, FALSE),length(x[ok]),replace=T, prob=c(percent,1-percent))
+}
+  if (black == TRUE) {
+    bl <- data$black.ei[ok] >.65}
+  if (hispanic==TRUE){
+ his <- data$hisp.ei[ok] > .65}
+  if (white==TRUE){
+ whit <- data$whit.ei[ok] > .65
+ }
+ none <- !bl & !his & !whit
+#print(sum(ok2))
+  if (informative==TRUE) {
+     ok2 <- bounds[,2]-bounds[,1] <.15 | bounds[,4]-bounds[,3] < .15
+}
+  for(i in 1:n){
+    if (bl[i] == TRUE & rand[i]==TRUE)  lines(bbounds[i,], wbounds[i,], col="red", lwd=1)
+    if (whit[i] == TRUE & rand[i]==TRUE)  lines(bbounds[i,], wbounds[i,], col="green", lwd=1)
+    if (his[i] == TRUE & rand[i]==TRUE)  lines(bbounds[i,], wbounds[i,], col="blue", lwd=1)
+  if (none[i] == TRUE & rand[i]==TRUE)  lines(bbounds[i,], wbounds[i,], col="black", lwd=1)
+  }
+  betabcd <- apply(betabs,1,function(x) quantile(x, probs=c(.1,.9)))
+  betabm <- apply(betabs,1, mean)
+  betawcd <- apply(betaws,1,function (x) quantile(x,probs=c(.1,.9)))
+  betawm <- apply(betaws,1, mean)
+  #n <- dim(betabcd)[2]
+  for(i in 1:n){
+    if (rand[i]==TRUE) {lines(betabcd[,i], sort(betawcd[,i],decreasing=T), col="yellow",lwd=1.5)
+    	}
+    if(random==FALSE){
+    points(betabm, betawm, col="yellow", cex=1, pch=16)
+    }
+  }
+  #if (random==TRUE) text(.85,.97,paste("Unanimous",unan))
+  legend(.69,1,c("White", "Black", "Hispanic", "Mixed", "CI of Estimates"), col=c("green", "red", "blue", "black", "yellow"), lwd=1)
+}
+
